10ea58dadb60a4e5a4cb632a8575af611da8a06b
unknown <brunnernikolaj@gmail.com>
2016-05-17 22:11:38 +0200

    profile view things

--- app/src/main/java/com/example/emil/taskmanager/fragments/ProfilePropertyFragment.java
+++ app/src/main/java/com/example/emil/taskmanager/fragments/ProfilePropertyFragment.java
@@ -21,17 +21,15 @@ import com.example.emil.taskmanager.R;
  * create an instance of this fragment.
  */
 public class ProfilePropertyFragment extends Fragment {
-    // FUCK
-    private TextView text = (TextView) getView().findViewById(R.id.profile_property_title);
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TITLE = "title";
+    private static final String VALUE = "value";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String mTitle;
+    private String mValue;
 
     private OnFragmentInteractionListener mListener;
 
@@ -43,16 +41,16 @@ public class ProfilePropertyFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param title Parameter 1.
+     * @param value Parameter 2.
      * @return A new instance of fragment ProfilePropertyFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ProfilePropertyFragment newInstance(String param1, String param2) {
+    public static ProfilePropertyFragment newInstance(String title, String value) {
         ProfilePropertyFragment fragment = new ProfilePropertyFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(TITLE, title);
+        args.putString(VALUE, value);
         fragment.setArguments(args);
         return fragment;
     }
@@ -61,16 +59,25 @@ public class ProfilePropertyFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mTitle = getArguments().getString(TITLE);
+            mValue = getArguments().getString(VALUE);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_profile_property, container, false);
+
+        TextView titleText = (TextView) view.findViewById(R.id.profile_property_title);
+        titleText.setText(mTitle);
+
+        TextView valueText = (TextView) view.findViewById(R.id.profile_property_value);
+        valueText.setText(mValue);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_property, container, false);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -111,14 +118,5 @@ public class ProfilePropertyFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
-
-    public void setPropertyTitle(String title) {
-        this.text.setText(title);
-    }
-
-    public void setPropertyValue() {
-
-    }
-
 
 }
